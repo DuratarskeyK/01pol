46d1361c71af66cff7af271ff594b455d3d2f525
lichak <lichak@cnode2.(none)>
2013-08-26 16:33:24 +0400

    Gzip support for facet counting, 7d stats

--- tools/build.sh
+++ tools/build.sh
@@ -2,5 +2,5 @@
 
 gcc -O2 -Wall -o incidence incidence.c lrs/lrslib.c lrs/lrsmp.c
 gcc -O2 -Wall -o dupe dupe.c
-mpicc -O2 -Wall -o facetcnt facetcnt.c lrs/lrslib.c lrs/lrsmp.c
+mpicc -O2 -Wall -lz -o facetcnt facetcnt.c lrs/lrslib.c lrs/lrsmp.c
 mpicc -O2 -Wall -o find_pol1 find_pol1.c lrs/lrslib.c lrs/lrsmp.c
